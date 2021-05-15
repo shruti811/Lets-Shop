39e953d33a0b013a393a7abd4e9dc29a79502d9a
Shruti Gupta <shrutig85923@gmail.com>
2021-05-16 01:18:24 +0530

    cart - add and remove

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,9 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home-page'),
-    path('checkout/',views.checkout,name='checkout'),
-    path('product/<slug>/',views.ItemDetailView.as_view(),name='product')
+    path('checkout/', views.checkout, name='checkout'),
+    path('product/<slug>/', views.ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/',
+         views.remove_from_cart, name='remove-from-cart')
 ]
