5baa8ff2e783645c5784b4a7475db3c33e37c602
Shruti Gupta <shrutig85923@gmail.com>
2021-05-16 14:51:14 +0530

    UI improvement

---
+++ core/templatetags/cart_template_tags.py
@@ -0,0 +1,14 @@
+from django import template
+from core.models import Order
+
+register = template.Library()
+
+
+@register.filter
+def cart_item_count(user):
+    if user.is_authenticated:
+        query_set = Order.objects.filter(user=user, ordered=False)
+        if query_set.exists():
+            return query_set[0].items.count()
+    else:
+        return 0
