a9c2002ce61ff14c5a1497d4055470af3042a8ec
Shruti Gupta <shrutig85923@gmail.com>
2021-05-19 23:10:17 +0530

    Add coupons

--- lets-shop/urls.py
+++ lets-shop/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', include('core.urls', namespace='core'))
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL,
-#                           document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
